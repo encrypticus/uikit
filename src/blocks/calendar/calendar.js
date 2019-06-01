7474814bd08ea99969029c941496a883a770e88e
Charli Santiago <encrypticus@gmail.com>
2019-06-01 21:35:09 +0600

    Начал кастомизацию элемента calendar.

--- src/blocks/calendar/calendar.js
+++ src/blocks/calendar/calendar.js
@@ -1,19 +1,34 @@
-import "./datepicker.min";
+import "./datepicker";
 
 $.fn.datepicker.language['en'] =  {
-  days: ['Sunday','Mondey','Вторник','Среда','Четверг','Пятница','Суббота'],
-  daysShort: ['Sun','Mon','Вто','Сре','Чет','Пят','Суб'],
-  daysMin: ['Su','Mo','Вт','Ср','Чт','Пт','Сб'],
-  months: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+  days: ['Sunday','Mondey','Tuesday','Wednesday','Thursday','Friday','Saturday'],
+  daysShort: ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
+  daysMin: ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
+  months: ['January','February','March','April','May','June','July','August','September','October','November','December'],
   monthsShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
-  today: 'Сегодня',
+  today: 'Today',
   clear: 'Очистить',
   dateFormat: 'dd.mm.yyyy',
   timeFormat: 'hh:ii',
   firstDay: 1
 };
 
+var $datepickerHeadres = $(".datepicker-header");
+$datepickerHeadres.text(new  Date().getDate());
+
 $('.datepicker-here').datepicker({
   language: 'en',
-  todayButton: new Date()
+  todayButton: new Date(),
+  onSelect: function (formattedDate, date, inst) {
+    var $datepickerHeader = $(".datepicker-header", inst.el);
+    $datepickerHeader.text(date.getDate());
+  },
+  navTitles: {
+    days: 'MM',
+  },
+  prevHtml: '<svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" viewBox="0 0 10 16"><g><g><path fill="#fff" d="M7.76.237L9.441 1.92 3.522 7.84l5.92 5.921-1.683 1.683L.185 7.871l.03-.03-.03-.03z"/></g></g></svg>',
+  nextHtml: '<svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" viewBox="0 0 10 16"><g><g><path fill="#fff" d="M9.815 7.811l-.03.03.03.03-7.574 7.574-1.683-1.683 5.92-5.921L.559 1.92 2.24.237z"/></g></g></svg>'
 });
+
+
+
