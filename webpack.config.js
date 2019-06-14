a21fcc56b7acb9a16418f8baed0e0ec45a2aedcb
Charli Santiago <encrypticus@gmail.com>
2019-06-14 14:05:59 +0600

    Рефакторинг кода.
    Отформатировал код в stages.scss. В webpack.config.js для объявления переменных сделал через единственную инструкцию var.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,20 @@
-const path = require("path");
-const merge = require("webpack-merge"); // объединяет массивы и объекты конфигураций из нескольких файлов-модулей
-
-const pug = require("./webpack/loaders/pug"); // модуль обработки pug-файлов
-const sass = require("./webpack/loaders/sass"); // модуль обработки sass|scss-файлов
-const miniCssExtractPlugin = require("./webpack/plugins/miniCssExtractPlugin");// плагин обработки css-файлов
-const sourceMap = require("./webpack/options/sourceMap"); // опция генерации карты кода
-const optimization = require("./webpack/options/optimization"); // опция оптимизации css- js-кода
-const watch = require("./webpack/options/watch"); // опция наблюдения за файлами для пересборки
-const font = require("./webpack/loaders/font"); // модуль обработки файлов шрифтов
-const htmlWebpackPlugin = require("./webpack/plugins/htmlWebpackPlugin");// модуль генерирует html-файл в папке сборки
-const cleanWebpackPlugin = require("./webpack/plugins/cleanWebpackPlugin"); // модуль очищает папку сборки перед пересборкой
-const styleLintPlugin = require("./webpack/plugins/styleLintPlugin"); // линтер стилевых файлов
-const browserSync = require("./webpack/plugins/browserSyncPlugin"); // в комментариях не нуждается
-const jquery = require("./webpack/plugins/jquery"); // плагин, добавляющий jquery в проект
-const css = require("./webpack/loaders/css"); // модуль обработки css-файлов
-const image = require("./webpack/loaders/image"); // модуль обработки файлов изображений
-const video = require("./webpack/loaders/video"); // модуль обработки видеофайлов
+const path = require("path"),
+    merge = require("webpack-merge"), // объединяет массивы и объекты конфигураций из нескольких файлов-модулей
+    pug = require("./webpack/loaders/pug"), // модуль обработки pug-файлов
+    sass = require("./webpack/loaders/sass"), // модуль обработки sass|scss-файлов
+    miniCssExtractPlugin = require("./webpack/plugins/miniCssExtractPlugin"),// плагин обработки css-файлов
+    sourceMap = require("./webpack/options/sourceMap"), // опция генерации карты кода
+    optimization = require("./webpack/options/optimization"), // опция оптимизации css- js-кода
+    watch = require("./webpack/options/watch"), // опция наблюдения за файлами для пересборки
+    font = require("./webpack/loaders/font"), // модуль обработки файлов шрифтов
+    htmlWebpackPlugin = require("./webpack/plugins/htmlWebpackPlugin"),// модуль генерирует html-файл в папке сборки
+    cleanWebpackPlugin = require("./webpack/plugins/cleanWebpackPlugin"), // модуль очищает папку сборки перед пересборкой
+    styleLintPlugin = require("./webpack/plugins/styleLintPlugin"), // линтер стилевых файлов
+    browserSync = require("./webpack/plugins/browserSyncPlugin"), // в комментариях не нуждается
+    jquery = require("./webpack/plugins/jquery"), // плагин, добавляющий jquery в проект
+    css = require("./webpack/loaders/css"), // модуль обработки css-файлов
+    image = require("./webpack/loaders/image"), // модуль обработки файлов изображений
+    video = require("./webpack/loaders/video"); // модуль обработки видеофайлов
 
 // функция вторым аргументом принимает args.mode от прописанных в package.json скриптов: args.mode = development или args.mode = production
 module.exports = (env, args) => {
