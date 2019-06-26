fb2f894f2ffbcfe0b64643282b58c10dc73fa56d
Charli Santiago <encrypticus@gmail.com>
2019-06-26 18:32:53 +0600

    Перенес некоторые блоки страницы из папки components в папку blocks. Добавил в блок контента.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './styles.scss';
-import './components/menu-toggler/menu-toggler';
+import './blocks/menu-toggler/menu-toggler';
 import './components/button/button';
 import './components/arrow-button/arrow-button';
-import './components/rotator/rotator';
+import './blocks/rotator/rotator';
