aa4f00d39b77bb95cc4ef08838f3541205e2ab39
Charli Santiago <encrypticus@gmail.com>
2019-06-28 12:01:04 +0600

    Добавил блок video

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import './blocks/menu-toggler/menu-toggler';
 import './components/button/button';
 import './components/arrow-button/arrow-button';
 import './blocks/rotator/rotator';
+import './components/air-videoplayer/air-videoplayer';
