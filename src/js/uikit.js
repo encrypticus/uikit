e26fedc42293ba21f5657d7be74c5ad1a9a2947c
Charli Santiago <encrypticus@gmail.com>
2019-07-15 19:30:52 +0600

    Добавил footer и header на страницу uikit.html

--- src/js/uikit.js
+++ src/js/uikit.js
@@ -8,3 +8,5 @@ import '../components/text-field/textfield';
 import '../components/location/location';
 import '../components/calendar/calendar';
 import '../components/air-videoplayer/air-videoplayer';
+import '../blocks/menu-toggler/menu-toggler';
+import '../blocks/footer/footer';
